5a62d329d418cbe295608e1f72954821b583f1c1
GabrieleSalati <gabriele.salati5@gmail.com>
2023-04-18 17:23:12 +0200

    migration added

--- database/migrations/2023_04_18_142251_create_projects_table.php
+++ database/migrations/2023_04_18_142251_create_projects_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->string('slug', 100)->unique();
+            $table->text('description');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
