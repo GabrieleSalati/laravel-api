7485445f2f506746d193f43f0cad11559ecf1c6a
GabrieleSalati <gabriele.salati5@gmail.com>
2023-04-26 15:50:24 +0200

    Problem on edit

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -15,7 +15,7 @@
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
                 <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
-                    value="{{ $project->title }}">
+                    value="{{ old('title', $project->title) }}">
                 @error('title')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -27,7 +27,7 @@
                 <select name="technology_id" id="technology_id" class="form-select" aria-label="Default select example">
                     <option selected>Select tech</option>
                     @foreach ($technologies as $technology)
-                        <option @if (old('techonology_id', $project->technology_id) == $technology->id)  @endif value="{{ $technology->id }}">
+                        <option @if (old('techonology_id', $project->technology_id) == $technology->id)  @endif value="{{ old('technology', $technology->id) }}">
                             {{ $technology->label }}</option>
                     @endforeach
                 </select>
@@ -36,7 +36,8 @@
                 <label for="types" class="form-label">Type</label>
                 @foreach ($types as $type)
                     <input class="form-check-control" type="checkbox" name="types[]" id="types - {{ $type->id }}"
-                        value="{{ $type->id }}"><label for="types" class="form-label">{{ $type->label }}</label>
+                        value="{{ $type->id }}"><label for="types"
+                        class="form-label">{{ old('types', $type->label) }}</label>
                 @endforeach
                 @error('types')
                     <div class="invalid-feedback">
@@ -47,7 +48,7 @@
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
                 <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description"
-                    name="description" rows="3">{{ $project->description }}</textarea>
+                    name="description" rows="3">{{ old('description', $project->description) }}</textarea>
                 @error('description')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -57,7 +58,7 @@
             <div class="mb-3">
                 <label for="link" class="form-label">Link</label>
                 <input type="text" class="form-control @error('link') is-invalid @enderror" id="link" name="link"
-                    value="{{ $project->link }}">
+                    value="{{ old('link', $project->link) }}">
                 @error('link')
                     <div class="invalid-feedback">
                         {{ $message }}
